fcb199fe6af78603d8db8bfa8670c67f07753312
Mr.Browser <140371660+browse780@users.noreply.github.com>
2023-07-24 12:31:13 +0700

    Add files via upload

---
+++ _2048_main.py
@@ -0,0 +1,15 @@
+from _2048_ai import *
+
+def main():
+    # Giving time necessary to change focus to the browser
+    time.sleep(3)
+
+    # Playing the game.
+    # Have fun experimenting on your own and try changing heuristics in grid.py to see the difference in performance :)
+    while 1:
+        updateGrid()
+        createMiniMaxTree(2)
+        performMove(getMoves())
+
+if __name__ == '__main__':
+    main()
